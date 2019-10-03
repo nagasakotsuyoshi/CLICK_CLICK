4bb485c59f67264bbaa9f0414dbc750020246927
nagasakotsuyoshi <naga2443b.o.c4649@gmail.com>
2019-10-03 22:57:12 +0900

    resultのスクリプト編集

--- Assets/Scripts/ResultButtonScript.cs
+++ Assets/Scripts/ResultButtonScript.cs
@@ -7,15 +7,6 @@ using UnityEngine.UI;
 
 public class ResultButtonScript : MonoBehaviour
 {
-    public GameObject resultObject = null;
-
-    void Start()
-    {
-        Text resultText = resultObject.GetComponent<Text>();
-        resultText.fontSize = 24;
-        resultText.text = "もう一回！";
-
-    }
     public void OnClick()
     {
         SceneManager.LoadScene("GameScene");
