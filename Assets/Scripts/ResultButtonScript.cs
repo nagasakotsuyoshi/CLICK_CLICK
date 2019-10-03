c24bdbac10cc4fa2b40bf6e3d5fca0c31fd85bf8
nagasakotsuyoshi <naga2443b.o.c4649@gmail.com>
2019-10-03 22:39:18 +0900

    結果画面追加

---
+++ Assets/Scripts/ResultButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ResultButtonScript : MonoBehaviour
+{
+    public GameObject resultObject = null;
+
+    void Start()
+    {
+        Text resultText = resultObject.GetComponent<Text>();
+        resultText.fontSize = 24;
+        resultText.text = "もう一回！";
+
+    }
+    public void OnClick()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
---
+++ Assets/Scripts/ResultTextScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultTextScript : MonoBehaviour
+{
+    public GameObject resultObject = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text resultText = resultObject.GetComponent<Text>();
+        resultText.fontSize = 24;
+        resultText.text = "結果：aaaaaaa\nスコア：";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /*if (Input.GetMouseButton(0))
+        {
+            SceneManager.LoadScene("GameScene");
+        }*/
+    }
+}
